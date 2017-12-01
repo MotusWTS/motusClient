4923a2bcbae06cc477de0d7485e7a1d4a3498a9f
Denis Lepage <dlepage@bsc-eoc.org>
2017-11-30 22:32:18 -0500

    getRunsFilters now returns the actual filter records. listRunsFilterds returns a list of all the existing filters in a dataframe

--- R/getRunsFilters.R
+++ R/getRunsFilters.R
@@ -1,6 +1,12 @@
-#' Returns a dataframe of the filters stored in the local database. 
+#' Returns a dataframe of the runFilters records matching a filter name (and optionally a project ID) stored 
+#' in the local database. 
 #'
 #' @param src dplyr sqlite src, as returned by \code{dplyr::src_sqlite()}
+#'
+#' @param filterName unique name given to the filter 
+#'
+#' @param motusProjID optional project ID attached to the filter in order to share with other users of the 
+#' same project.
 #'
 #' @return a dataframe 
 #'
@@ -11,7 +17,11 @@
 getRunsFilters <- function(src, filterName, motusProjID=NA) {
   
   sqlq = function(...) DBI::dbGetQuery(src$con, sprintf(...))
-  
-  return (sqlq("select * from filters"))
+
+  filterID = getRunsFilterID(src, filterName, motusProjID)
+  if (!is.null(filterID)) {
+      return (sqlq("select * from runsFilters where filterID = '%d'", filterID))
+  }
+  return()
   
 }
