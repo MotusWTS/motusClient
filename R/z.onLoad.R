f4bb11d396d83d2cc89599b8cc9aa5eba7202414
Denis Lepage <dlepage@bsc-eoc.org>
2018-01-09 21:03:10 -0500

    server for master is sandbox

--- R/z.onLoad.R
+++ R/z.onLoad.R
@@ -47,7 +47,7 @@
 .onLoad = function(...) {
     ## interim location of unprotected local data server
 
-    dataServerURL = "https://beta.motus.org/api/sgdata"
+    dataServerURL = "https://sandbox.motus.org/api/sgdata"
     ##dataServerURL = "https://sgdata.motus.org/data"
 
 
